8b3c15c8bbce7a646c6eef2597ac5ddfc38147b6
Olivia <olivia.fintina@gmail.com>
2025-12-15 03:42:20 +0200

    Small improvement to the initialization script

--- init.sh
+++ init.sh
@@ -3,3 +3,8 @@
 python3 -m venv wifi_analyzer
 source wifi_analyzer/bin/activate
 pip install -r requirements.txt
+pip install --upgrade pip
+
+cd src/scripts
+chmod 755 monitor_mode.sh
+chmod 755 managed_mode.sh
